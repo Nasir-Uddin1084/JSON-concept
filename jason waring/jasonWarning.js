6c91b06943d65bf773cab4c20b6305d02bd44d0a
Nasir Uddin Khan <uddin6058@gmail.com>
2021-09-29 12:15:20 +0600

    JSON Warning

---
+++ jason waring/jasonWarning.js
@@ -0,0 +1,16 @@
+// JSON did not support hardCore JSON object
+// HardCore json 
+// it will give us warning
+var person1 = {
+	"name": 'nasir',
+	"age": 23,
+	"homeTwon": 'Noakhali',
+	"married": false,
+};
+console.log(person1); 
+// var person1_json = JSON.stringify(person1); // JSON wil convert it object
+// console.log(person1_json);
+// var person1_obj = JSON.parse(person1_json);// JSON wil convert it string
+// console.log(person1_obj);
+
+
